ce03350254462cf2000d2d898469dde5f048dd94
Pannn <130228018+Panncreas@users.noreply.github.com>
2024-08-13 10:10:02 +0700

    api url

--- src/utils/imageURL.js
+++ src/utils/imageURL.js
@@ -1,6 +1,6 @@
 // const imgUrl = 'http://37.128.248.23:50458/downloadFile/';
- const imgUrl = 'http://localhost:8081/api';
-// const imgUrl = 'http://192.168.100.12:8080/downloadFile/';
+ const imgUrl = 'http://114.9.13.243:8081/api';
+// const imgUrl = 'http://localhost:8081/api/';
 
 
 export default imgUrl;
