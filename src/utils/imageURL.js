703f55172208c04b8a6b6c71a7bcdfd081244781
shevahikam <shevasparta1927@gmail.com>
2024-12-21 23:34:56 +0700

    Menggabungkan perubahan terbaru

--- src/utils/imageURL.js
+++ src/utils/imageURL.js
@@ -1,5 +1,5 @@
 // const imgUrl = 'http://37.128.248.23:50458/downloadFile/';
-const imgUrl = "http://192.168.2.255:8081/api";
+const imgUrl = "http://192.168.100.148:8081/api";
 // const imgUrl = 'http://localhost:8081/api/';
 
 export default imgUrl;
